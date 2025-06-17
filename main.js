0dc1908aaa3911206f3ab03662f466f74e0b6fc2
AlineRodrigues02 <alinemariarodrigues08@gmail.com>
2025-06-17 14:26:05 -0300

    Fiz atualizações de como será salvo no localStorag

--- main.js
+++ main.js
@@ -1,24 +1,15 @@
+// aqui vai criar um array para o paciene adicionado
+let paciente = JSON.parse(localStorage.getItem("paciente")) || []
 //Enviando a triagem 
 function gerarTriagem(){
     const nome = document.getElementById("nome").value
     const motivo = document.getElementById("motivo").value
     const novoPaciente = {nome,motivo}
-    // aqui vai criar um array para o paciene adicionado
-    let paciente = JSON.parse(localStorage.getItem("paciente")) || []
     paciente.push(novoPaciente)
     localStorage.setItem("paciente", JSON.stringify(paciente))
     alert("Cadastrado com sucesso")
 
 }
-
-//Mostra oque tem no cadastro(do localStorage) na triagem
-
-const nome = localStorage.getItem("nome")
-const motivo = localStorage.getItem("motivo")
-document.getElementById("nomeExibido").textContent = nome ?? "Paciente não colocado"
-document.getElementById("SintomaExibido").textContent = motivo ?? "Sintomas não colocado"
-
-
 
 //campo de triagem
 
