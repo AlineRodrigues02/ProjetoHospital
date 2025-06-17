6e64c20cda85d8085e3a73c09f61a4fb005c4ef4
AlineRodrigues02 <alinemariarodrigues08@gmail.com>
2025-06-17 13:43:42 -0300

    Novas funções colocadas para funcionar, gerar triagem, função enviar triagem e armazenanar no localStorage

---
+++ main.js
@@ -0,0 +1,32 @@
+//Enviando a triagem 
+function gerarTriagem(){
+    const nome = document.getElementById("nome").value
+    const motivo = document.getElementById("motivo").value
+    localStorage.setItem("nome", nome)
+    localStorage.setItem("motivo", motivo)
+    alert("Cadastrado com sucesso")
+}
+
+//Mostra oque tem no cadastro(do localStorage) na triagem
+
+const nome = localStorage.getItem("nome")
+const motivo = localStorage.getItem("motivo")
+document.getElementById("nomeExibido").textContent = nome ?? "Paciente não colocado"
+document.getElementById("SintomaExibido").textContent = motivo ?? "Sintomas não colocado"
+
+//campo de triagem
+
+function enviarTriagem(){
+    const paciente = {
+        temperatura: document.getElementById("temperatura").value,
+        pressao: document.getElementById("pressao").value,
+        observacao: document.getElementById("observacao").value,
+        glicemia: document.getElementById("glicemia").value, 
+
+        riscos: document.getElementById("risco").value,
+    }
+
+    //armazenado dados
+    localStorage.setItem("dadosTriagem",JSON.stringify(paciente));
+    alert("dados enviados para o médico")
+}
