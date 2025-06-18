a0855a69769459e37698e03489ba102b31f9cf96
AlineRodrigues02 <alinemariarodrigues08@gmail.com>
2025-06-18 11:22:46 -0300

    criei a parte funcional da tela do médico para atendimento

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function gerarTriagem() {
     localStorage.setItem("paciente", JSON.stringify(paciente))
 
     alert("Cadastrado com sucesso")
-    window.location.href = "../Triagem de Pacientes/index.html"
+    // window.location.href = "../Triagem de Pacientes/index.html"
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -48,3 +48,69 @@ function enviarTriagem() {
 
   alert("Dados enviados para o médico");
 }
+
+//parte da tela do médico
+
+document.addEventListener("DOMContentLoaded", () => {
+  const filaEl = document.getElementById("filaPacientes")
+  if (!filaEl) return
+
+  let paciente = JSON.parse(localStorage.getItem("paciente")) || [];
+
+  //mostrar os pacientes que ja passaram pela triagem
+
+  const comTriagem = paciente.filter(p => p.triagem)
+
+  if(comTriagem.length === 0){
+    filaEl.innerHTML = "<p>Nenhum paciente aguardando atendimento</p>"
+    return;
+  }
+  //Organizar a prioridade 
+  const prioridade ={
+    vermelho:1,
+    amarelo: 2,
+    verde:3
+  }
+  //ordenar
+  comTriagem.sort((a,b)=>{
+    return prioridade[a.triagem.risco] - prioridade[b.triagem.risco]
+  })
+
+  //mostrar a Lista
+  comTriagem.forEach((paciente,index)=>{
+    const cor = paciente.triagem.risco
+
+    const corDeRisco = {
+      verde: "🟢",
+      amarelo: "🟡",
+      vermelho: "🔴"
+      
+    }[cor.toLowerCase()] || "⚪"
+
+    const div = document.createElement("div")
+    div.innerHTML = `
+      <strong>Paciente:</strong> ${paciente.nome} <br>
+      <strong>Risco:</strong> ${corDeRisco} ${cor} <br>
+      <button onclick="atenderPaciente(${index})">Atender</button>
+      <hr>`
+
+      filaEl.appendChild(div)
+  })
+})
+  
+//remove paciente da fila ao ser atendido
+
+function atenderPaciente(index){
+   let paciente = JSON.parse(localStorage.getItem("paciente")) || [];
+   const comTriagem = paciente.filter(p => p.triagem)
+
+   const atendido = comTriagem[index]
+   alert(`atendendo paciente: ${atendido.nome}`)
+   //remover da fila  original
+
+   const novaFila = paciente.filter(p => p !== atendido)
+   localStorage.setItem("paciente",JSON.stringify(novaFila))
+
+  //Atualizar a tela
+  location.reload();
+   }
